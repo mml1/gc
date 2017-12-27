8cb2c16ea5fa843d92aa70aad9b843d3ae5b7d73
mml1 <mmlliguicota@gmail.com>
2017-12-26 18:21:41 -0800

    Created donor rows with info from req and added total

--- js/request.js
+++ js/request.js
@@ -1,10 +1,21 @@
     function donorDataRequest() {
-    	console.log('in req')
     	var xhttp = new XMLHttpRequest();
     	xhttp.onreadystatechange = function() {
-    		console.log('run')
     		if (this.readyState == 4 && this.status == 200) {
-    			console.log(this.responseText);
+    			var data = JSON.parse(this.responseText)
+    			var donorFormatted = [];
+    			var total = 0;
+    			for (var i = 0; i < data.length; i++) {
+    				total += data[i].amount;
+    				donorFormatted.push('\n<div class="table-row"> \n \t <div class="col-4">' + data[i].name + '</div> <div class="col-4">$' + data[i].amount + '</div> <div class="col-4">' + data[i].type + '</div></div>')
+
+    			}
+
+                // Using data from json req to create donor table rows and total
+    			document.getElementById("donors").innerHTML = donorFormatted.join('');
+    			document.getElementById("total").innerHTML = '<h1 class="college-header_1">$' + total + '</h1>';
+                document.getElementById("small-total").innerHTML = '$' + total + ' ';
+
 
     		}
     	};
